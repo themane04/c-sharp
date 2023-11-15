55a23e7d68c64c49107bc4b31fe3b7f46fca42a8
Marjan <marjan@tie-international.com>
2023-11-15 08:28:07 +0100

    neue Aufgaben

--- Kara/Form1.SteuerungKara.cs
+++ Kara/Form1.SteuerungKara.cs
@@ -8,15 +8,20 @@ namespace KaraProjekt
         private void Main(string args)
         {
             //Hier die Anweisungen notieren
-            AvoidTree();
-            TurnAround();
-            AvoidTree();
-            TurnAround();
+            // AvoidTree(); - Aufgabe 1.3
+            /* - Aufgabe 1.4
+            AvoidTreeRight();
+            AvoidTreeLeft();
+            AvoidTreeRight();
+            */
+            
             //Programmende: nächste Zeile bitte nicht löschen
             MessageBox.Show("Programmcode ausgeführt!");
         }
 
         //Hier können Sie eigene Methoden einfügen
+        
+        /* - Aufgabe 1.3
         private void AvoidTree()
         {
             kara.TurnLeft();
@@ -26,13 +31,38 @@ namespace KaraProjekt
             kara.Move();
             kara.TurnRight();
             kara.Move();
-            kara.Move();
         }
-
+        */
         private void TurnAround()
         {
             kara.TurnLeft();
             kara.TurnLeft();
         }
+
+        /*   Aufgabe 1.4
+        private void AvoidTreeRight()
+        {
+            kara.TurnRight();
+            kara.Move();
+            kara.TurnLeft();
+            kara.Move();
+            kara.Move();
+            kara.TurnLeft();
+            kara.Move();
+            kara.TurnRight();
+        }
+
+        private void AvoidTreeLeft()
+        {
+            kara.TurnLeft();
+            kara.Move();
+            kara.TurnRight();
+            kara.Move();
+            kara.Move();
+            kara.TurnRight();
+            kara.Move();
+            kara.TurnLeft();
+        }
+        */
     }
 }
